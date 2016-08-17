7a5b87ac9e91d119a0740b739afbeb1864c86d4b
David Jennes <david.jennes@appwise.be>
2016-08-17 19:31:33 +0200

    initial commit

---
+++ Test/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  Test
+//
+//  Created by David Jennes on 03/05/16.
+//  Copyright © 2016. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+	var window: UIWindow?
+}
+
---
+++ Test/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by David Jennes on 03/05/16.
+//  Copyright © 2016. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPopoverPresentationControllerDelegate {
+	var wrapInNavigationController: Bool = true
+
+	override func viewDidLoad() {
+		NSLog("========================")
+	}
+
+	deinit {
+		NSLog("Deinit main view controller")
+	}
+
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		if let popover = segue.destinationViewController.popoverPresentationController {
+			popover.delegate = self
+		}
+
+		if let identifier = segue.identifier where identifier.hasPrefix("no wrap in navigation controller") {
+			wrapInNavigationController = false
+		} else {
+			wrapInNavigationController = true
+		}
+
+		NSLog("--- Showing details ---")
+	}
+
+	func presentationController(controller: UIPresentationController, viewControllerForAdaptivePresentationStyle style: UIModalPresentationStyle) -> UIViewController? {
+		if (wrapInNavigationController) {
+			return NavigationController(rootViewController: controller.presentedViewController)
+		} else {
+			return controller.presentedViewController
+		}
+	}
+}
+
+class NavigationController: UINavigationController {
+	deinit {
+		NSLog("Deinit navigation controller")
+	}
+}
+
+class DetailsViewController: UIViewController {
+	@IBAction func dismiss(button: UIButton) {
+		dismissViewControllerAnimated(true, completion: nil)
+	}
+
+	deinit {
+		NSLog("Deinit details view controller")
+	}
+}
